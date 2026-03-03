16b19a9ed3feaa31acf9a6e1ff33b11f10a6d843
David Krstov <davidkrstov@yahoo.com>
2026-03-03 23:17:41 +0100

    Initial commit

---
+++ src/test/java/com/pronajdiusluga/app/PronajdiUslugaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pronajdiusluga.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PronajdiUslugaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
